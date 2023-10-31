cc175461a8a2a9c2bdf921692d2a846a4511ffc4
sebinchu0218 <sebinchu0218@gmail.com>
2023-10-31 17:11:28 +0900

    commit

---
+++ src/test/java/com/example/dockercicdtest/DockerCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockercicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
