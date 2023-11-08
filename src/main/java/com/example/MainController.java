636e1ed1159a96b33c9e0ce9ce0463790cbfe08b
추세빈 <cntpqls0218@gmail.com>
2023-11-08 09:40:18 +0900

    간단한 웹 html 올리기

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,19 +1,19 @@
 package com.example;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+
+@Controller
 public class MainController {
 
     @GetMapping("hello")
-    public String getHello() {
-        return "hi";
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
     }
-
-
-
 }
